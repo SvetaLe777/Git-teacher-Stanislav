97fbb8a9d432251b1b156151556d6c4dc8124566
Svetlana <svetale7773@gmail.com>
2022-12-25 11:54:53 +0300

    добавила нг

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
 print ("2023")
 
+![новый год](https://yandex.ru/images/search?text=%D0%97%D0%B8%D0%BC%D0%BD%D1%8F%D1%8F%20%D0%A1%D0%BA%D0%B0%D0%B7%D0%BA%D0%B0&nl=1&source=morda&pos=5&rpt=simage&img_url=http%3A%2F%2Fproza.ru%2Fpics%2F2021%2F12%2F17%2F242.jpg&lr=213)
+
+
+
+
+
